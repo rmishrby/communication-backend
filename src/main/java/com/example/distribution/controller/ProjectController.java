96ad5990627951a4376c6bc7eb75db877166b8f2
rmishrby <rkmishra226022@gmail.com>
2025-05-14 20:16:49 +0530

    feature-3 | added search api to get username

--- src/main/java/com/example/distribution/controller/ProjectController.java
+++ src/main/java/com/example/distribution/controller/ProjectController.java
@@ -19,6 +19,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/projects")
+@CrossOrigin
 public class ProjectController {
 
     private static final Logger logger = LoggerFactory.getLogger(ProjectController.class);
--- src/main/java/com/example/distribution/controller/UserSearchController.java
+++ src/main/java/com/example/distribution/controller/UserSearchController.java
@@ -17,6 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
+@CrossOrigin
 @Validated
 public class UserSearchController {
 
